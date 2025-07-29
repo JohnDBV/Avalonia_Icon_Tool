bbec38e68486cb93aa1c3ae0c0b9a522d99ca99c
Ionut Dogaru <Ionut.Dogaru@insightsoftware.com>
2025-07-29 17:47:56 +0300

    First Commit, empty window

---
+++ TestWPFIconTool/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TestWPFIconTool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
